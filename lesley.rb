8e15b5b9fdefd97b0a72a374be60191a19f6e1c9
Rigoberto Molina <rigothedev@gmail.com>
2018-07-07 01:13:26 -0700

    More learning!

--- lesley.rb
+++ lesley.rb
@@ -19,9 +19,6 @@ else
   puts "I am beautiful"
 end
 
-def multiply_by_10
-end
-
 def give player, item, number
   puts "Gave #{player} #{number} #{item}s"
 end
@@ -41,3 +38,112 @@ def kill target, killer
 end
 
 kill 'Rigo', 'Lesley'
+
+# def function_name value1, value2
+#   puts "HI I AM A FUNCTION, #{value1}"
+# end
+
+# function_name 24, "dadsd"
+# function_name 25, "sdasd"
+# function_name 56, "dasd"
+
+def enchant item
+  puts "Enchanted #{item}"
+end
+
+enchant 'sword'
+
+def weather type
+  puts "Weather changed to #{type}"
+end
+
+weather 'Rainy'
+weather 'Sunny'
+weather 'Windy'
+
+def gamemode mode
+  if mode == 0
+    puts "gamemode changed to survival"
+  else
+    puts "gamemode changed to creative"
+  end
+end
+
+gamemode 9
+
+def shoot target, shooter
+  puts "#{target} was shot by #{shooter}"
+end
+
+shoot 'Rigo', 'fortune cookie'
+shoot 'skeleton', 'Lesley'
+
+
+
+
+
+def multiply_by_5 value
+  puts "#{value * 5}"
+end
+
+multiply_by_5 10
+
+
+def add_3 value
+  puts "#{value + 3}"
+end
+
+add_3 4
+
+
+
+
+def divide_by_2 value
+  puts "#{value / 2}"
+end
+
+divide_by_2 100
+
+
+
+def subtract_7 value
+  puts "#{value - 7}"
+end
+subtract_7 50
+
+
+
+def years_until_adult age
+  puts "#{18 - age} more years until I am an adult"
+end
+years_until_adult 12
+years_until_adult 16
+
+def years_until_can_drink age
+  puts "#{21 - age} more years until I can drink"
+end
+years_until_can_drink 16
+years_until_can_drink 12
+
+
+def best_country person
+  if person == 'Rigo'
+    puts "Its okish but the president is cheap and stingy"
+  else
+    puts "This is the best country we are very rich"
+  end
+end
+
+best_country 'Rigo'
+best_country 'Lesley'
+
+
+def best_drink drink
+  if drink == 'Coffe'
+    puts "code in disguise without it no technology"
+  else
+    puts "My voice medicine and relaxation drink"
+  end
+end
+
+best_drink 'Coffe'
